920b227283dcd031c4d3500c0af9345bd9560639
AvivAbachi <AvivAbachi@Gmail.com>
2020-11-03 13:55:52 +0200

    fix nav and nav btn

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,18 +1,23 @@
 import React, { FC, memo } from 'react';
-import { useLocation } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 import { NavBarProps } from './index.d';
 
 const NavBar: FC<NavBarProps> = ({ children, title, whitLocation, prefixLocation }) => {
+	const { pathname } = useLocation();
+	const classColor = pathname === '/hooks' ? ' hooks' : pathname === '/mobx' ? ' mobx' : pathname === '/redux' ? ' redux' : '';
+
 	const Location = () => {
-		const { pathname } = useLocation();
-		return pathname === '/' ? '' : pathname.replace(/\//, prefixLocation ?? ' ');
+		const path = pathname === '/' ? '' : pathname.replace(/\//g, prefixLocation ?? ' ');
+		return path;
 	};
 	return (
 		<nav className='navbar'>
 			{title && (
-				<h1 className='navbar__title '>
-					{title}
-					{whitLocation && Location()}
+				<h1 className={`navbar__title${classColor}`}>
+					<Link to='/'>
+						{title}
+						{whitLocation && Location()}
+					</Link>
 				</h1>
 			)}
 			{children && <ul className='nav_menu'>{children}</ul>}
--- src/components/NavBtn.tsx
+++ src/components/NavBtn.tsx
@@ -2,9 +2,9 @@ import React, { FC, memo } from 'react';
 import { Link } from 'react-router-dom';
 import { NavBtnProps } from './index.d';
 
-const NavBtn: FC<NavBtnProps> = ({ to = '/', label }) => {
+const NavBtn: FC<NavBtnProps> = ({ to = '/', label, className }) => {
 	return (
-		<li className='nav_btn'>
+		<li className={`nav_btn ${className ?? ''}`}>
 			<Link to={to} className='nav_btn__link'>
 				{label}
 			</Link>
