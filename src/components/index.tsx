f30508908796f92e17177019d08f1e24b2813fb7
AvivAbachi <AvivAbachi@Gmail.com>
2020-11-04 11:20:24 +0200

    add all new components components

--- src/components/index.tsx
+++ src/components/index.tsx
@@ -1,11 +1,14 @@
 export { default as Btn } from './Btn';
+export { default as BtnFetch } from './BtnFetch';
 export { default as Card } from './Card';
 export { ObserverCard } from './Card';
 export { default as Empty } from './Empty';
 export { default as List } from './List';
 export { ObserverList } from './List';
+export { default as LogoLink } from './LogoLink';
 export { default as NavBar } from './NavBar';
 export { default as NavBtn } from './NavBtn';
 export { default as Page } from './Page';
 export { default as Radio } from './Radio';
 export { default as RadioGroup } from './RadioGroup';
+export { default as Toolbar } from './Toolbar';
